f66b7fafc831ce9c30ffca501d4453957056d461
007jag <114781974+007jag@users.noreply.github.com>
2023-02-23 17:57:33 -0500

    fixed refactored code bugs

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,26 +1,24 @@
 const router = require('express').Router();
-const { createNewNote, updateDb } = require('../../lib/notes');
+const {createNewNote, updateDb} = require("../../lib/notes");
 const { v4: uuidv4 } = require('uuid');
-const { notes } = require('../../db/db.json');
+const {notes} = require("../../db/db.json");
 
-router.get('/notes', (req, res) => {
-  res.json(notes);
-});
 
-router.post('/notes', (req, res) => {
-  const newNote = {
-    id: uuidv4(),
-    title: req.body.title,
-    text: req.body.text,
-  };
-  createNewNote(newNote, notes);
-  res.json(newNote);
-});
+router.get("/notes", (req, res) => {
+    let results = notes;
+    res.json(results);
+  });
 
-router.delete('/notes/:id', (req, res) => {
-  const noteId = req.params.id;
-  updateDb(noteId, notes);
-  res.sendStatus(204);
-});
+    router.post("/notes", (req, res) => {
+    req.body.id = uuidv4();
+    const newNote = createNewNote(req.body, notes);
+    res.json(newNote);
+  });  
 
-module.exports = router;
+  router.delete("/notes/:id" , (req, res) => {
+    const params = req.params.id
+    updateDb(params, notes);
+    res.redirect('');
+  });
+
+  module.exports = router;
--- server.js
+++ server.js
@@ -1,18 +1,16 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
-
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
+    console.log(`API server now on port ${PORT}. Welcome!`);
+  });
