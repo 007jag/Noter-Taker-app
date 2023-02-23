dddaeb17b5f2e1dd243ecc9224c8e253b3693b12
007jag <114781974+007jag@users.noreply.github.com>
2023-02-23 17:41:52 -0500

    restructured few things

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
-const express = require("express");
-const app = express();
-const PORT = process.env.PORT || 3001;
+const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+
 app.use(express.static('public'));
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}. Welcome!`);
-  });
+  console.log(`Server is listening on port ${PORT}`);
+});
+
