6d40773bec26a93b9585e1708de8ee1b4dc7dc42
007jag <114781974+007jag@users.noreply.github.com>
2023-02-23 14:57:02 -0500

    added api and html routes code

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const path = require("path");
 const fs = require("fs");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const apiRoutes = require('./routes/apiRoutes/APIroutes');
-const htmlRoutes = require('./routes/htmlRoutes/HTMLroutes');
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 app.use(express.static('public'));
 app.use(express.urlencoded({extended: true}));
